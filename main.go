7ae6289f5e2be07021a9580121446bbe37e4cb29
Bimal2614 <Bimal2614@gmail.com>
2024-03-30 10:53:54 +0530

    add middleware, limiter 3003

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bimal2614/ginBoilerplate/database"
 	"github.com/bimal2614/ginBoilerplate/src/endpoints"
+	limiter "github.com/davidleitw/gin-limiter"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
 )
 
-// call initDB function from database/init_db.go
+func ResponseTimeMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Start timer
+		startTime := time.Now()
+
+		// Process request
+		c.Next()
+
+		// Calculate response time
+		endTime := time.Now()
+		latency := endTime.Sub(startTime)
+
+		// log the user IP, request method, status code, and latency
+		fmt.Printf("IP: %s - EndTime: [%v] %v %v %v\n", c.ClientIP(), endTime.Format("2006-01-02 15:04:05"), c.Request.Method, c.Request.URL.Path, latency)
+	}
+}
 
 func main() {
 
@@ -18,22 +36,29 @@ func main() {
 		fmt.Println("No .env file found")
 	}
 
+	rdb := redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
+	dispatcher, err := limiter.LimitDispatcher("24-M", 100, rdb)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 	// gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
 
 	// Mount routes from different endpoint groups
+	router.Use(ResponseTimeMiddleware())
 	api := router.Group("/api")
 	{
 		// Mount routes from all endpoints.go
-		endpoints.SetupUserRoutes(api)
+		endpoints.SetupUserRoutes(api, dispatcher)
 		endpoints.SetupWebsocketRoutes(router)
 	}
 
 	// Initialize the database connection
-	err := database.InitDB()
-	if err != nil {
-		panic(err)
+	err_ := database.InitDB()
+	if err_ != nil {
+		panic(err_)
 	}
 
 	router.Run(":8080")
--- src/endpoints/users.go
+++ src/endpoints/users.go
@@ -2,17 +2,19 @@ package endpoints
 
 import (
 	"github.com/bimal2614/ginBoilerplate/src/controllers"
+	limiter "github.com/davidleitw/gin-limiter"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupUserRoutes(router *gin.RouterGroup) {
+func SetupUserRoutes(router *gin.RouterGroup, dispatcher *limiter.Dispatcher) {
 
 	userRoutes := router.Group("/v1")
 
 	userController := controllers.NewUserController()
 	{
-		userRoutes.POST("/login", userController.Login)
-		userRoutes.POST("/register", userController.Register)
+		//  add rate limiter
+		userRoutes.POST("/login", dispatcher.MiddleWare("1-m", 10), userController.Login)
+		userRoutes.POST("/register", dispatcher.MiddleWare("1-d", 20), userController.Register)
 		userRoutes.POST("/verify-otp", userController.VerifyOTP)
 	}
 
